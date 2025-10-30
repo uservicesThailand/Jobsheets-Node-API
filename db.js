406ce7e42777db6c42b4164995e8855dcadf6845
Pimlapas Yasavoot <Pimlapas.y@UST.inf>
2025-10-30 11:02:50 +0700

    add endpiont for pm form

--- db.js
+++ db.js
@@ -15,10 +15,49 @@ const db = mysql.createPool({
   }
 });
 
+// Database connection pool 2
+const db2 = mysql.createPool({
+  host: process.env.DB2_HOST || process.env.DB_HOST, // ใช้ host เดียวกันหรือต่างกันได้
+  user: process.env.DB2_USERNAME || process.env.DB_USERNAME,
+  password: process.env.DB2_PASSWORD || process.env.DB_PASSWORD,
+  database: process.env.DB2_DATABASE, // database name ต่างกัน
+  connectionLimit: 10,
+  connectTimeout: 20000, // 20 วินาที
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const db3 = mysql.createPool({
+  host: process.env.DB3_HOST,
+  user: process.env.DB3_USERNAME,
+  password: process.env.DB3_PASSWORD,
+  database: process.env.DB3_DATABASE,
+  connectionLimit: 10,
+  connectTimeout: 20000, // 20 วินาที
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 // Optional: handle error globally
 db.on('error', (err) => {
   console.error('Database error:', err.code);
 });
 
+
+db2.on('error', (err) => {
+  console.error('Database 2 error:', err.code);
+});
+
+
+db3.on('error', (err) => {
+  console.error('Database 2 error:', err.code);
+});
+
 // รองรับ callback
-module.exports = db;
+module.exports = {
+  db,
+  db2,
+  db3
+};
