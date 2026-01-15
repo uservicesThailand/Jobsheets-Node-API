5f7d65b246c614d4cdd81b2c5e24210e89aa6fbf
thana.sa <thana.s@uservices-thailand.com>
2026-01-15 13:38:01 +0700

    docs: API form mechanical service

--- docs/index.js
+++ docs/index.js
@@ -5,5 +5,6 @@ module.exports = {
     ...require("./form/balance/shaft.doc"),
     ...require("./form/balance/field.doc"),
     ...require("./form/coil/coil.doc"),
+    ...require("./form/mechanical-service/mechanical.doc"),
   },
 };
